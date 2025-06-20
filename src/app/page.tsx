b622c9faf0ab40b4761d5db51c03332ea1a22022
Oleksii <oleksii.shevchenko92@gmail.com>
2025-06-20 16:59:29 +0300

    add dinamic force

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { SectionBlogs } from "@/components/sectionBlog/sectionBlog";
 import { AddBlog } from "@/components/ButtonAddBlog";
 import { getBlogsRoute } from "@/routes/getBlogsRoute";
-
+export const dynamic = "force-dynamic";
 const Home = async () => {
   const blogs = await getBlogsRoute();
   return (
