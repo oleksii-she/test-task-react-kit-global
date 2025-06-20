4d633ad6b1effb66a6bf808c568dcb3d882453d0
Oleksii <oleksii.shevchenko92@gmail.com>
2025-06-20 16:44:56 +0300

    delete redux section blogs

--- src/components/sectionBlog/sectionBlog.tsx
+++ src/components/sectionBlog/sectionBlog.tsx
@@ -1,21 +1,22 @@
-"use client";
-import { useEffect } from "react";
-import { useAppSelector } from "@/store/hooks";
+/* import { useEffect } from "react"; */
+
 import { BlogList } from "../blogList/BlogList";
 
-import { useAppDispatch } from "@/store/hooks";
-import { getBlogsApi } from "@/store/features/blogSlice";
+/* import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { getBlogsApi } from "@/store/features/blogSlice"; */
 import { IBlog } from "@/types";
-export const SectionBlogs = ({ blogs }: { blogs: IBlog[] }) => {
-  const items = useAppSelector((state) => state.blogs.blogs);
 
-  const dispatch = useAppDispatch();
-  useEffect(() => {
+export const SectionBlogs = ({ blogs }: { blogs: IBlog[] }) => {
+  /*   const items = useAppSelector((state) => state.blogs.blogs); */
+
+  /*   const dispatch = useAppDispatch(); */
+  /*   useEffect(() => {
     dispatch(getBlogsApi(blogs));
   }, [blogs, dispatch]);
+ */
   return (
     <section>
-      <BlogList items={items} />
+      <BlogList items={blogs} />
     </section>
   );
 };
