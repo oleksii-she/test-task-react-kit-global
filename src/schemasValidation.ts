c0245c50b6322b5ad07e22324a678667d03f6332
Oleksii <oleksii.shevchenko92@gmail.com>
2025-06-22 17:55:39 +0300

    fix validation, fix modal

--- src/schemasValidation.ts
+++ src/schemasValidation.ts
@@ -2,23 +2,37 @@ import { z } from "zod";
 
 export const addBlogSchema = z.object({
   id: z.string(),
-  title: z.string().min(3, "The title must contain at least 3 characters"),
+  title: z
+    .string()
+    .min(3, "The title must contain at least 3 characters")
+    .max(45, "The title must contain at least 45 characters"),
   description: z.string().min(5, "The description should be more expanded"),
 });
 
 export const addCommentSchema = z.object({
   id: z.string(),
-  author: z.string().min(3, "The NickName must contain at least 3 characters"),
+  author: z
+    .string()
+    .min(3, "The NickName must contain at least 3 characters")
+    .max(20, "The NickName must contain at least 20 characters"),
   text: z.string().min(5, "The description should be more expanded"),
 });
 
 export const addCommentSchemaNoId = z.object({
-  author: z.string().min(3, "The NickName must contain at least 3 characters"),
+  author: z
+    .string()
+    .min(3, "The NickName must contain at least 3 characters")
+    .max(20, "The NickName must contain at least 20 characters"),
   text: z.string().min(5, "The description should be more expanded"),
 });
 
 export const updateCommentSchema = z.object({
   id: z.string(),
-  author: z.string().min(3).optional(),
+  author: z
+    .string()
+    .min(3)
+    .max(20, "The NickName must contain at least 20 characters")
+    .optional(),
+
   text: z.string().min(5).optional(),
 });
